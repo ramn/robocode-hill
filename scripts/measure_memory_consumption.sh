54841e209c5b37476cc2f4ff6a58dacea5243b99
ramn <github@rymdimperiet.org>
2015-03-12 21:42:29 +0100

    Memory consumption script: both mac and linux

--- scripts/measure_memory_consumption.sh
+++ scripts/measure_memory_consumption.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-while true; do { pgrep -f /Library/Java | xargs ps -ocpu,rss,command -p ;} ; sleep 1; done
+if [ $(uname) == "Linux" ]; then
+    JAVA_PREFIX="$HOME/lib/jdk1"
+else
+    JAVA_PREFIX="/Library/Java"
+fi
+
+while true; do { pgrep -f "$JAVA_PREFIX" | xargs ps -orss,command -p ;} ; sleep 1; done
