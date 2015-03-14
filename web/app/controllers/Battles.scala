b67ef05474dd86dcb0041ee97e89f1aabecfa888
ramn <github@rymdimperiet.org>
2015-03-14 23:46:14 +0100

    Link to bot page from battle report

--- web/app/controllers/Battles.scala
+++ web/app/controllers/Battles.scala
@@ -4,6 +4,7 @@ import play.api._
 import play.api.mvc._
 import se.ramn.roborunner.RandomBattleground
 import se.ramn.models.BattleReport
+import se.ramn.models.BattleReportView
 import se.ramn.models.SuccessfulBattle
 import se.ramn.models.FailedBattle
 
@@ -16,7 +17,8 @@ object Battles extends Controller {
   def create = Action { request =>
     val battleReport = RandomBattleground.run
     battleReport match {
-      case battleReport: SuccessfulBattle =>
+      case successfulBattle: SuccessfulBattle =>
+        val battleReport = new BattleReportView(successfulBattle)
         Ok(views.html.battles.show(battleReport))
       case failedBattle: FailedBattle =>
         InternalServerError("500 - Failed to run battle")
---
+++ web/app/models/BattleReportView.scala
@@ -0,0 +1,25 @@
+package se.ramn.models
+
+import robocode.control.events.BattleCompletedEvent
+import robocode.control.BattleSpecification
+import robocode.BattleResults
+
+
+case class ScoreRecord(bot: Bot, battleResults: BattleResults)
+
+
+class BattleReportView(report: SuccessfulBattle) {
+  val SuccessfulBattle(
+    battleRequest,
+    RobocodeBattleReport(completedEvent, battleSpecification)
+  ) = report
+
+  private val indexedResults = completedEvent.getIndexedResults
+
+  require(battleRequest.bots.size == indexedResults.size)
+
+  val scoreRecordsByScore =
+    (battleRequest.bots zip indexedResults)
+    .map(ScoreRecord.tupled)
+    .sortBy(_.battleResults.getScore)
+}
