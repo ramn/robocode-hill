877c6c89458c086fb882e30f0e87aa4e720850ae
ramn <github@rymdimperiet.org>
2015-03-18 22:19:07 +0100

    Log selected db dir and bot dir

--- web/app/controllers/Global.scala
+++ web/app/controllers/Global.scala
@@ -1,9 +1,13 @@
 import play.api._
 
+import se.ramn.Config
+
 
 object Global extends GlobalSettings {
   override def onStart(app: Application): Unit = {
     Logger.info("Application onStart callback")
+    Logger.info(s"Using DbFile: ${Config.DbFile}")
+    Logger.info(s"Using BotDir: ${Config.BotDir}")
   }
 
   override def onStop(app: Application): Unit = {
