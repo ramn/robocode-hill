16b1985c3d871465f34eac2a85c376d066f4853a
ramn <github@rymdimperiet.org>
2015-03-26 23:06:23 +0100

    Add robocode.sh GUI runner
    With scala-library added to classpath

---
+++ scripts/robocode.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#  -DWORKINGDIRECTORY=<path>  Set the working directory
+#  -DROBOTPATH=<path>         Set the robots directory (default is 'robots')
+#  -DBATTLEPATH=<path>        Set the battles directory (default is 'battles')
+#  -DNOSECURITY=true|false    Enable/disable Robocode's security manager
+#  -Ddebug=true|false         Enable/disable debugging used for preventing
+#                             robot timeouts and skipped turns, and allows an
+#                             an unlimited painting buffer when debugging robots
+#  -DlogMessages=true|false   Log messages and warnings will be disabled
+#  -DlogErrors=true|false     Log errors will be disabled
+#  -DEXPERIMENTAL=true|false  Enable/disable access to peer in robot interfaces
+#  -DPARALLEL=true|false      Enable/disable parallel processing of robots turns
+#  -DRANDOMSEED=<long number> Set seed for deterministic behavior of random
+#                             numbers
+
+SCALA_LIB="/users/ramn/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.11.6.jar"
+
+pwd=`pwd`
+cd "${0%/*}"
+java -Xmx512M -DlogMessages=true -DlogErrors=true -Ddebug=true -cp libs/robocode.jar:"$SCALA_LIB" robocode.Robocode $*
+cd "${pwd}"
