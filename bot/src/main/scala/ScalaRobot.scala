ffd860a9769fef18e4df42e28aaa3f4283327fa9
ramn <github@rymdimperiet.org>
2015-03-26 23:34:40 +0100

    Add robocode properties file, use proper package
    Properties file specifies main class.  Required for Robocode to be able
    to load a jar file.

--- bot/src/main/scala/ScalaRobot.scala
+++ bot/src/main/scala/ScalaRobot.scala
@@ -1,17 +1,35 @@
-package sample
+package se.ramn.ramnbot
+
+import java.awt.Color
+import scala.util.Random
 
 import robocode.HitByBulletEvent
-import robocode.Robot
+import robocode.AdvancedRobot
 import robocode.ScannedRobotEvent
+import robocode.util.Utils.normalRelativeAngleDegrees
 
-class ScalaRobot extends Robot {
-  override def run() = {
-		while (true) {
-			ahead(100)
-			turnGunRight(360)
-			back(100)
-			turnGunRight(360)
-		}
+
+class RamnBot extends AdvancedRobot {
+  override def run = {
+    // Can we load Scala classes? Else null pointer exception
+    out.println(scala.math.BigInt(1))
+
+    setAdjustGunForRobotTurn(true) // Keep the gun still when we turn
+    setAdjustRadarForGunTurn(true)
+    setAdjustRadarForRobotTurn(true)
+
+    setColors(
+      new Color(0, 100, 255),
+      new Color(255, 0, 0),
+      new Color(0, 0, 0))
+
+    while (true) {
+      execute()
+      setTurnRadarRight(360)
+      setTurnRight(360)
+      setAhead(10000)
+      setFire(1)
+    }
   }
 
   override def onScannedRobot(e: ScannedRobotEvent) = {
